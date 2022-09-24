da24f2063d7f599f3c123560c60a37374afcc721
Manuel-Greulich <manuelgreulich@yahoo.com>
2022-09-24 15:32:21 +0200

    finished small changes

--- src/app/main-section/main-section.component.ts
+++ src/app/main-section/main-section.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare let anime: any; 
 
 @Component({
   selector: 'app-main-section',
   templateUrl: './main-section.component.html',
   styleUrls: ['./main-section.component.scss']
 })
-export class MainSectionComponent implements OnInit {
+export class MainSectionComponent implements AfterViewInit {
 
   constructor() { }
+
+
+  ngAfterViewInit(): void {
+
+    // setTimeout(() => {
+    anime.timeline()
+    .add({
+      targets: '.c2 .word',
+      scale: [14,1],
+      opacity: [0,1],
+      easing: "easeOutCirc",
+      duration: 1000,
+      delay: (el, i) =>  800 * i
+
+    }).add({
+      targets: '.c2',
+      opacity: [1],
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000,
+    });
+    // }, 200);
+  }
 
   ngOnInit(): void {
   }
